dfec072d52eed2b4b017ea98a190262d70229a26
Yumi Minami <yumiminami@Yumis-MacBook-Pro.local>
2019-05-22 10:03:49 -0700

    search bar

--- public/search.js
+++ public/search.js
@@ -1,1 +1,94 @@
-import * as functions from 'firebase-functions';
+$('document').ready(function(){
+  $('#searchbutton').click(() => {
+    $('ul').empty();
+    const petData = document.querySelector('#pet-list');
+
+    // create element and render pet list
+    function renderName(doc) {
+      let li = document.createElement('li');
+      let name = document.createElement('span');
+
+      let dateLabel = document.createElement('span');
+      let date = document.createElement('span');
+
+      let locationLabel = document.createElement('span');
+      let location = document.createElement('span');
+
+      let rewardLabel = document.createElement('span');
+      let reward = document.createElement('span');
+
+      var img = document.createElement("img");
+
+
+
+      li.setAttribute('data-id', doc.id);
+
+      name.textContent = doc.data().name.toUpperCase();
+
+      dateLabel.textContent = "DATE";
+      date.textContent = doc.data().date;
+
+      locationLabel.textContent = "LOCATION";
+      location.textContent = doc.data().location;
+
+      rewardLabel.textContent = "REWARD";
+      reward.textContent = doc.data().reward;
+      img.src = doc.data().picture;
+
+
+      li.appendChild(name);
+
+      li.appendChild(dateLabel);
+      li.appendChild(date);
+
+      li.appendChild(locationLabel);
+      li.appendChild(location);
+
+      li.appendChild(rewardLabel);
+      li.appendChild(reward);
+
+      li.appendChild(img);
+
+
+
+      petData.appendChild(li);
+    }
+    db.collection('petTest').get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+        let name = doc.data().name;
+        let location = doc.data().location;
+        let status = doc.data().status;
+        let search = $('#searchBox').val();
+        name = name.toUpperCase();
+        search = search.toUpperCase();
+        if (name == search) {
+          renderName(doc);
+        }
+        else if (search== ""){
+          renderName(doc);
+        }
+      })
+    });
+    db.collection('pets').get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+        let name = doc.data().name;
+        let location = doc.data().location;
+        let status = doc.data().status;
+        let search = $('#searchBox').val();
+        name = name.toUpperCase();
+        search = search.toUpperCase();
+        if (name == search) {
+          renderName(doc);
+        }
+        else if (name && (search== "")){
+          renderName(doc);
+        }
+      })
+    });
+  })
+  $('#searchBox').keypress(function(e){
+       if(e.which == 13){//Enter key pressed
+           $('#searchbutton').click();//Trigger search button click event
+       }
+   });
+});
